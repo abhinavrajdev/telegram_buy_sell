83bae4bd1391f4078667ac4d89a6373d9c557ffd
Abhinav Raj <150703845+abhinavrajdev@users.noreply.github.com>
2024-09-24 13:59:21 +0530

    Delete telegram_utils directory

--- telegram_utils/commands.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const { search, trending, price } = require("./bot_commands");
-const initializeBotCommands = (bot) => {
-    bot.command("buy_token", search);
-};
-module.exports = { initializeCommands };
